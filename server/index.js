70400c072819f768fd8127252cdd0d443ab5c1d9
rvitality <vital.renzjohn@gmail.com>
2023-03-08 12:14:40 +0800

    Add other CRUD functions

--- server/index.js
+++ server/index.js
@@ -24,6 +24,41 @@ app.get("/api/list", (req, res) => {
     });
 });
 
+app.post("/api/list/add", (req, res) => {
+    const q = "INSERT INTO todo(`title`, `status`) VALUES (?)";
+    const values = [req.body.title, req.body.status];
+
+    db.query(q, [values], (err, data) => {
+        if (err) return res.status(500).json(err);
+
+        return res.status(200).json(data);
+    });
+});
+
+app.put("/api/list/:id", (req, res) => {
+    const { id } = req.params;
+    const q = "UPDATE  todo SET `title` = ?, `status` = ? WHERE `id` = ?";
+    const values = [req.body.title, req.body.status];
+
+    db.query(q, [...values, id], (err, data) => {
+        if (err) return res.status(500).json(err);
+
+        return res.status(200).json("Item has been updated!");
+    });
+});
+
+app.delete("/api/list/:id", (req, res) => {
+    const { id } = req.params;
+
+    const q = "DELETE FROM todo WHERE `id` = ?";
+
+    db.query(q, [id], (err, data) => {
+        if (err) return res.status(500).json(err);
+
+        return res.status(200).json("Item has been deleted.");
+    });
+});
+
 app.listen(8800, () => {
     console.log("Connected to server...");
 });
